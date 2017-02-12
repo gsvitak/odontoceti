c59b0d85919117eeb3112699b2f8b7d450715a78
Gregory Foster <gregoryfoster@tests-MacBook-Pro-3.local>
2017-02-12 15:26:48 -0500

    Initial Commit

---
+++ Odontoceti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Odontoceti
+//
+//  Created by Gregory Foster on 2/12/17.
+//  Copyright © 2017 Greg M Foster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
